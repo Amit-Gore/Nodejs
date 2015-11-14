020a109fd2558af6dd6550ea8993d6d3ef96a675
Amit Gore <amitgore2009@gmail.com>
2015-11-14 18:08:36 +0530

    installing express js,sample testing etc

---
+++ app_01/server.js
@@ -0,0 +1,34 @@
+/*// get the http and filesystem modules
+ var http = require('http'),
+    fs = require('fs');
+  
+  // create our server using the http module
+  http.createServer(function(req, res) {
+  
+    // write to our server. set configuration for the response
+    res.writeHead(200, {
+     'Content-Type': 'text/html',
+     'Access-Control-Allow-Origin' : '*'
+   });
+ 
+   // grab the index.html file using fs
+   var readStream = fs.createReadStream(__dirname + '/index.html');
+ 
+   // send the index.html file to our user
+   readStream.pipe(res);  
+ 
+ }).listen(1337);
+ 
+ // tell ourselves what's happening
+ console.log('Visit me at http://localhost:1337');*/
+
+var express = require('Express');
+var app = express();
+var path = require('path');
+
+app.get('/',function(req,res)
+       {
+        res.sendFile(path.join(__dirname+'/index.html')); 
+});
+app.listen(1337);
+console.log("1337 is a magic port :D");
